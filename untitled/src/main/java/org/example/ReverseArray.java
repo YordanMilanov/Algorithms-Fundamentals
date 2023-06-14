af21576062860fa2fc961fc236f8b00f650e8eec
YordanMilanov <dancho.galinov@gmail.com>
2023-06-14 16:19:31 +0300

    task 2 completed

--- untitled/src/main/java/org/example/ReverseArray.java
+++ untitled/src/main/java/org/example/ReverseArray.java
@@ -3,9 +3,8 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
-public class Main {
+public class ReverseArray {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
